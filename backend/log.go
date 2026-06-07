c7dd473d0ccbbadc7beef30215e2fe5a757dc093
tree <tree@tree.fail>
2026-06-07 15:06:18 +0200

    fix: update timestamp format in Logf function to use RFC3339Nano

--- backend/log.go
+++ backend/log.go
@@ -8,6 +8,6 @@ import (
 
 // Logf writes a timestamped line to stderr for the background backend log.
 func Logf(format string, args ...any) {
-	ts := time.Now().Format("2006-01-02 15:04:05.000")
+	ts := time.Now().UTC().Format(time.RFC3339Nano)
 	fmt.Fprintf(os.Stderr, "%s %s\n", ts, fmt.Sprintf(format, args...))
 }
